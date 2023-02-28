c759528dbc168cea12291be7f4041968ecfd6787
Simon Ármin <arminsimon5@gmail.com>
2023-02-28 12:00:02 +0100

    1)a feladat

---
+++ gitgyakorlas-csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitgyakorlas_csharp
+{
+    internal class Program
+    {
+        static int nagyobb(int szam1, int szam2)
+        {
+            if (szam1 < szam2)
+            {
+                return szam2;
+            }
+            else if (szam2 < szam1)
+            {
+                return szam1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
