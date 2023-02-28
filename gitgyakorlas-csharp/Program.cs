a5ee42658c65b3c78ed0c1e060d4548e6eaa4fd5
Simon Ármin <arminsimon5@gmail.com>
2023-02-28 12:16:00 +0100

    1)b feladat

--- gitgyakorlas-csharp/Program.cs
+++ gitgyakorlas-csharp/Program.cs
@@ -18,10 +18,19 @@ namespace gitgyakorlas_csharp
             {
                 return szam1;
             }
+            else { return szam1 - szam2; }
+
         }
 
         static void Main(string[] args)
         {
+            Console.Write("Adja meg az első számot.: ");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adja meg az második számot.: ");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            if (szam1 - szam2 == nagyobb(szam1, szam2)) { Console.WriteLine("A két szám egyenlő."); }
+            else { Console.WriteLine("A nagyobb szám a " + nagyobb(szam1, szam2)); }
+            Console.ReadKey();
         }
     }
 }
